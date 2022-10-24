d178c241e77979dbf6b161d7b4fb60f4c1e4cc6e
Prime010 <116540734+Prime010@users.noreply.github.com>
2022-10-24 13:57:30 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY1_ALVAREZ/ACTIVITY 1/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "Julian Prime Alvarez" ;
+        int age = 19;
+        float gpa=  1.80f;
+        String bloodtype = " O ";
+        
+        
+        System.out.println("My name is " + name + ".");
+        System.out.println("I am " + age + " years old. ");
+        System.out.println("My GPA is " + gpa + ".");
+        System.out.println("My blood type is" + bloodtype + ".");
+    }
+}
